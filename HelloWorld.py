5d176dc05c23381a4f4434f687756793450130b7
Sautul Mehta <sautulmehta@gmail.com>
2018-08-26 14:53:10 +0530

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+# this is a comment
+# In C#, we need to end the statement with semicolon, but anyhow we are going to indent our code and going to write a new statement on new Line. Do we really need it?? In Python, Not.
+
+print('Hello World')
+
+# And this will print Hello World in console. No class declaration needed for this :) In Visual Studio, we get this C# REPL option in interactive window. 
