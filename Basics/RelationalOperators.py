d4492544db2f568e3cf9db83b85411131505d2f4
Sautul Mehta <sautulmehta@gmail.com>
2018-08-26 15:14:05 +0530

    Create RelationalOperators.py

---
+++ Basics/RelationalOperators.py
@@ -0,0 +1,14 @@
+#Everything is simillar to c#
+
+# Greater than
+x > y
+#	Less than
+x < y
+#	Equal to
+x == y
+#	Not equal to
+x != y
+#	Greater than or equal to
+x >= y
+#	Less than or equal to
+x <= y
