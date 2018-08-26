59c4341458ae7e969e7be55987254cfff215c63b
Sautul Mehta <sautulmehta@gmail.com>
2018-08-26 15:22:04 +0530

    Create LogicalOperators.py

---
+++ Basics/LogicalOperators.py
@@ -0,0 +1,9 @@
+
+#	AND .. different from c# (&&) but simple :)
+x and y
+# OR .. In C# (||)
+x or y
+# NOT .. In C# (!)
+not x
+
+# in c# we are having small t in boolean true, in python, we are having True
