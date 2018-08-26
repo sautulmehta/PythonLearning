4833e19c722c925a2fa878366c40431f02638e51
Sautul Mehta <sautulmehta@gmail.com>
2018-08-26 15:09:17 +0530

    Create BasicsArithmeticOperations.py

---
+++ Basics/BasicsArithmeticOperations.py
@@ -0,0 +1,20 @@
+# Sum
+2 + 3
+# Multiplication
+2 * 3
+# Division
+2 / 3
+# subtraction
+2 - 3
+# Modulo
+3 % 2
+# These all have the same Syntaxes as we have in C#
+
+# The difference we are having in power
+# In C#, we need to use Math.Pow(a, b)
+# In Python ...
+2 ** 3
+
+# In Python, If we want to have the division rounded to floor ..
+5 // 2 
+# This will give 2, 2.5 is rounded to floor 2
